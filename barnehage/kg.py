14d017ac2f71882a17f8b6c223a8e6694a24d634
FanuelHab <fanhab123@gmail.com>
2024-10-30 11:33:09 +0100

    Update kg.py

--- barnehage/kg.py
+++ barnehage/kg.py
@@ -8,7 +8,15 @@ from kgmodel import (Foresatt, Barn, Soknad, Barnehage)
 from kgcontroller import (form_to_object_soknad, insert_soknad, commit_all, select_alle_barnehager)
 
 app = Flask(__name__)
-app.secret_key = 'BAD_SECRET_KEY' # nødvendig for session
+app.secret_key = 'BAD_SECRET_KEY'  # nødvendig for session
+def hent_ledige_plasser(barnehage_navn):
+    # Hent alle barnehager og deres ledige plasser
+    barnehager = select_alle_barnehager()
+    for barnehage in barnehager:
+        if barnehage.barnehage_navn == barnehage_navn:  # Endret til riktig attributtnavn
+            return barnehage.barnehage_ledige_plasser  # Bruker riktig attributtnavn for ledige plasser
+    return 0  # Returner 0 hvis barnehagen ikke finnes eller har ingen ledige plasser
+
 
 @app.route('/')
 def index():
@@ -27,29 +35,34 @@ def behandle():
         log = insert_soknad(form_to_object_soknad(sd))
         print(log)
         session['information'] = sd
-        return redirect(url_for('svar')) #[1]
+
+        # Hent valgt barnehage og sjekk ledige plasser
+        valgt_barnehage = sd.get("barnehage")
+        antall_ledige_plasser = hent_ledige_plasser(valgt_barnehage)
+        har_fortrinnsrett = bool(sd.get("fortrinnsrett", False))  # Sjekk om søkeren har fortrinnsrett
+
+        # Sett resultat basert på ledige plasser og fortrinnsrett
+        if antall_ledige_plasser > 0 or har_fortrinnsrett:
+            session['resultat'] = "TILBUD"
+        else:
+            session['resultat'] = "AVSLAG"
+
+        return redirect(url_for('svar'))
     else:
         return render_template('soknad.html')
 
 @app.route('/svar')
 def svar():
     information = session['information']
-    return render_template('svar.html', data=information)
+    resultat = session.get('resultat', 'Ingen respons tilgjengelig')
+    data = {
+        "resultat": resultat,
+        "søknadsdata": information
+    }
+    return render_template('svar.html', data=data)
 
 @app.route('/commit')
 def commit():
     commit_all()
     return render_template('commit.html')
 
-
-
-
-"""
-Referanser
-[1] https://stackoverflow.com/questions/21668481/difference-between-render-template-and-redirect
-"""
-
-"""
-Søkeuttrykk
-
-"""
